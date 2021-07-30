50cdc0bf0317fe6f85a90c100c6162880b681c07
phaeliks <87185033+phaeliks@users.noreply.github.com>
2021-07-30 02:10:45 +0200

    SOL > SAFE

--- libsol/printer_test.c
+++ libsol/printer_test.c
@@ -6,17 +6,17 @@ void test_print_amount() {
   char printed[24];
 
   print_amount(0, printed, sizeof(printed));
-  assert_string_equal(printed, "0 SOL");
+  assert_string_equal(printed, "0 SAFE");
   print_amount(1, printed, sizeof(printed));
-  assert_string_equal(printed, "0.000000001 SOL");
+  assert_string_equal(printed, "0.000000001 SAFE");
   print_amount(1000000000, printed, sizeof(printed));
-  assert_string_equal(printed, "1 SOL");
+  assert_string_equal(printed, "1 SAFE");
   print_amount(10000000000000001, printed, sizeof(printed));
-  assert_string_equal(printed, "10000000.000000001 SOL");
+  assert_string_equal(printed, "10000000.000000001 SAFE");
   print_amount(10000000001, printed, sizeof(printed));
-  assert_string_equal(printed, "10.000000001 SOL");
+  assert_string_equal(printed, "10.000000001 SAFE");
   print_amount(10000000100000000, printed, sizeof(printed));
-  assert_string_equal(printed, "10000000.1 SOL");
+  assert_string_equal(printed, "10000000.1 SAFE");
 }
 
 void test_print_token_amount() {
